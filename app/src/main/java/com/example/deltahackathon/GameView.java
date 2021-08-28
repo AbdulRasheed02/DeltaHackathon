4cd782fa825eb63a17300260334731f01e7660b6
Abdul Rasheed Mohamed Ali <abdulmdali@gmail.com>
2021-08-28 12:45:18 +0530

    Commit

--- app/src/main/java/com/example/deltahackathon/GameView.java
+++ app/src/main/java/com/example/deltahackathon/GameView.java
@@ -1,6 +1,7 @@
 package com.example.deltahackathon;
 
-import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.view.SurfaceView;
 
 public class GameView extends SurfaceView implements Runnable {
@@ -11,6 +12,14 @@ public class GameView extends SurfaceView implements Runnable {
     public static float screenRatioX, screenRatioY;
     private float screenX,screenY;
 
+    private BackGround backGround1,backGround2;
+    private Ninja ninja;
+
+    int ninjaRunFrame=1;
+
+    Paint paint;
+
+
     public GameView(GameActivity activity, float screenX, float screenY) {
         super(activity);
 
@@ -19,11 +28,71 @@ public class GameView extends SurfaceView implements Runnable {
         screenRatioX=2400f/screenX;
         screenRatioY=1080f/screenY;
 
+        backGround1=new BackGround(screenX,screenY,getResources());
+        backGround2=new BackGround(screenX,screenY,getResources());
+        backGround1.x=0;
+        backGround2.x=screenX;
+
+        ninja=new Ninja(getResources());
+
+
+        paint=new Paint();
+
     }
 
     @Override
     public void run() {
 
+        while(canDraw){
+
+        movement();
+        draw();
+
+        }
+
+    }
+
+    private void draw() {
+        if(getHolder().getSurface().isValid()){
+            Canvas canvas=getHolder().lockCanvas();
+
+            canvas.drawBitmap(backGround1.background,backGround1.x,backGround1.y,paint);
+            canvas.drawBitmap(backGround2.background,backGround2.x,backGround2.y,paint);
+
+            if(ninja.ninjaRun){
+                canvas.drawBitmap(ninja.getNinjaRun(ninjaRunFrame),ninja.x,ninja.y,paint);
+            }
+
+            getHolder().unlockCanvasAndPost(canvas);
+        }
+    }
+
+    private void movement() {
+        backGround();
+        ninja();
+    }
+
+    private void backGround() {
+        backGround1.x-=10*screenRatioX;
+        backGround2.x-=10*screenRatioX;
+
+        if(backGround1.x+backGround1.background.getWidth()<0){
+            backGround1.x=screenX;
+        }
+
+        if(backGround2.x+backGround2.background.getWidth()<0){
+            backGround2.x=screenX;
+        }
+    }
+
+    private void ninja() {
+        if(ninja.ninjaRun) {
+            if (ninjaRunFrame < 11) {
+                ninjaRunFrame++;
+            } else {
+                ninjaRunFrame = 1;
+            }
+        }
     }
 
     public void resume(){
--- app/src/main/java/com/example/deltahackathon/Ninja.java
+++ app/src/main/java/com/example/deltahackathon/Ninja.java
@@ -13,7 +13,7 @@ public class Ninja {
     float x,y,width,height;
     public static float base;
     Bitmap ninjaRun1,ninjaRun2,ninjaRun3,ninjaRun4,ninjaRun5,ninjaRun6,ninjaRun7,ninjaRun8,ninjaRun9,ninjaRun10;
-    Boolean atBase=true,ninjaJump=false,ninjaUpward=false,ninjaDownward=false;
+    Boolean atBase=true,ninjaRun=true,ninjaJump=false,ninjaUpward=false,ninjaDownward=false;
 
     Ninja(Resources res){
         ninjaRun1= BitmapFactory.decodeResource(res,R.drawable.run1);
@@ -30,26 +30,63 @@ public class Ninja {
         width=ninjaRun1.getWidth();
         height=ninjaRun1.getHeight();
 
-        width/=3;
-        height/=2;
+        width/=4;
+        height/=4;
 
         width*=screenRatioX;
         height*=screenRatioY;
 
         ninjaRun1=Bitmap.createScaledBitmap(ninjaRun1,(int)width,(int)height,false);
-        base=(900)*screenRatioY;
-        y=(900)*screenRatioY-height;
+        ninjaRun2=Bitmap.createScaledBitmap(ninjaRun2,(int)width,(int)height,false);
+        ninjaRun3=Bitmap.createScaledBitmap(ninjaRun3,(int)width,(int)height,false);
+        ninjaRun4=Bitmap.createScaledBitmap(ninjaRun4,(int)width,(int)height,false);
+        ninjaRun5=Bitmap.createScaledBitmap(ninjaRun5,(int)width,(int)height,false);
+        ninjaRun6=Bitmap.createScaledBitmap(ninjaRun6,(int)width,(int)height,false);
+        ninjaRun7=Bitmap.createScaledBitmap(ninjaRun7,(int)width,(int)height,false);
+        ninjaRun8=Bitmap.createScaledBitmap(ninjaRun8,(int)width,(int)height,false);
+        ninjaRun9=Bitmap.createScaledBitmap(ninjaRun9,(int)width,(int)height,false);
+        ninjaRun10=Bitmap.createScaledBitmap(ninjaRun10,(int)width,(int)height,false);
+
+        base=(1000)*screenRatioY;
+        y=base-height;
         x=150*screenRatioX;
 
     }
 
-    Bitmap getBall(){
-
-        return ninjaRun1;
+    Bitmap getNinjaRun(int ninjaRunFrame){
+        if(ninjaRunFrame==1){
+            return ninjaRun1;
+        }
+        else if(ninjaRunFrame==2){
+            return ninjaRun2;
+        }
+        else if(ninjaRunFrame==3){
+            return ninjaRun3;
+        }
+        else if(ninjaRunFrame==4){
+            return ninjaRun4;
+        }
+        else if(ninjaRunFrame==5){
+            return ninjaRun5;
+        }
+        else if(ninjaRunFrame==6){
+            return ninjaRun6;
+        }
+        else if(ninjaRunFrame==7){
+            return ninjaRun7;
+        }
+        else if(ninjaRunFrame==8){
+            return ninjaRun8;
+        }
+        else if(ninjaRunFrame==9){
+            return ninjaRun9;
+        }
+        else {
+            return ninjaRun10;
+        }
     }
 
     RectF getCollisionShape(){
-
         return new RectF(x,y,x+width,y+height);
     }
 
